4d87296ed6be84daf320acc7b0471ec0487c3892
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-19 20:51:41 -0600

    Add outputs as comments  into the file

--- example_3_3.rb
+++ example_3_3.rb
@@ -2,3 +2,10 @@ puts 'you\'re swell!'
 puts 'backslash at the end of a string:  \\'
 puts 'up\\down'
 puts 'up\down'
+
+# OUTPUT:
+#
+# you're swell!
+# backslash at the end of a string:  \
+# up\down
+# up\down
