0b526dc47dd295ba7183b100c81f9f2875476b3b
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-20 14:24:49 -0600

    Add more exmpl to the file

--- chapter6/example_6_1.rb
+++ chapter6/example_6_1.rb
@@ -14,6 +14,14 @@ name = gets.chomp
 puts 'Did you know there are ' + name.  length.to_s + ' characters'
 puts 'in your name, ' + name + '?'
 
+letters = 'aAbBcCdDeE'
+puts letters.upcase
+puts letters.downcase
+puts letters.swapcase
+puts letters.capitalize
+puts ' a'.capitalize
+puts letters
+
 # OUTPUT:
 #
 # pots
