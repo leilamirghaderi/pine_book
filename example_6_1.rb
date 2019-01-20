a159a06d1faa428e082aa3dd5fb1542a18e93b99
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-20 10:11:37 -0600

    Add example_6_1 to the repo

---
+++ example_6_1.rb
@@ -0,0 +1,19 @@
+var1 = 'stop'
+var2 = 'deliver rapid desserts'
+var3 = '....TCELES B HSUP A magic spell?'
+
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+puts var1
+puts var2
+puts var3
+
+# OUTPUT:
+#
+# pots
+# stressed dipar reviled
+# ?lleps cigam A PUSH B SELECT....
+# stop
+# deliver rapid desserts
+# ....TCELES B HSUP A magic spell?
