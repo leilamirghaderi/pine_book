23ec6d9632a0ea5f7d616c5cfa52543cdf07234a
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-19 22:38:17 -0600

    Change the code and add outs as comment to the file

--- example_5_6.rb
+++ example_5_6.rb
@@ -1,14 +1,42 @@
+# Question number one:
+
 puts 'Hi, what\'s your first name?'
-my_first_name = ' Leila'
-puts 'My firs name is'+ my_first_name + '.'
+first_name = gets.chomp
+puts 'So, your first name is '+ first_name + '.'
 puts 'What\'s your middle name?'
-my_middle_name = ' Sadat'
-puts 'My middle name is' + my_middle_name + '.'
+middle_name = gets.chomp
+puts 'So, your middle name is ' + middle_name + '.'
 puts 'What\'s your last name?'
-my_last_name = ' Mirghaderi'
-puts 'my last name is'+ my_last_name+'.'
-puts 'So, what\'s your full name?'
-puts 'My full name is' + my_first_name + my_middle_name + my_first_name + '.'
+last_name = gets.chomp
+puts 'So, your last name is '+ last_name+'.'
+puts 'Your full name is ' + first_name + ' ' + middle_name + ' ' + last_name + '.'
+puts 'What a beautiful name!'
+
+# OUTPUT:
+#
+# Leila
+# So, your first name is Leila.
+# What's your middle name?
+# Sadat
+# So, your middle name is Sadat.
+# What's your last name?
+# Mirghaderi
+# So, your last name is Mirghaderi.
+# Your full name is Leila Sadat Mirghaderi.
+# What a beautiful name!
+
+
+
+# Question number two:
+
 puts 'What\'s your favorite number?'
-favorite_number = '7'
-puts 'My favorite number is ' + favorite_number + '.'
+num = gets.chomp
+better = num.to_i + 1
+better = better.to_s
+puts 'This is bigger and better: ' + better + '.'
+
+# OUTPUT:
+#
+# What's your favorite number?
+# 6
+# This is bigger and better: 7.
