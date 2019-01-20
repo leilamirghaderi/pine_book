bf8f3bf80731cf9afcfb525493810cdd9d06af60
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-19 20:55:51 -0600

    Updates teh order of the  outputs as comments  into the file

--- example_2_4.rb
+++ example_2_4.rb
@@ -1,60 +1,14 @@
 puts 1.0 + 2.0
-
-# OUTPUT:
-#
-# 3.0
-
 puts 2.0 + 3.0
-
-# OUTPUT:
-#
-# 5.0
-
 puts 5.0 - 8.0
-
-# OUTPUT:
-#
-# -3.0
-
 puts 9.0 / 2.0
-
-# OUTPUT:
-#
-# 4.5
-
 puts 1+2
-
-# OUTPUT:
-#
-# 3
-
 puts 2*3
-
-# OUTPUT:
-#
-# 6
-
 puts 5-8
-
-# OUTPUT:
-#
-# -3
-
 puts 9/2
-
-# OUTPUT:
-#
-# 4
-
 puts 5 * (12-8)+ -15
-
-# OUTPUT:
-#
-# 5
-
 puts 98 + (59872 / (13*8)) * -51
 
 # OUTPUT:
 #
 # -29227
-  
