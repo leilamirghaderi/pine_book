a15365cb2f3b117aa78a21eddcfcd971c3776243
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-19 20:45:34 -0600

    Add outputs as comments  into the file

--- example_3_1.rb
+++ example_3_1.rb
@@ -2,3 +2,10 @@ puts 'I like' + 'apple pie.'
 puts 'I like ' + 'apple pie.'
 puts 'I like' + ' apple pie.'
 puts 'blink ' * 4
+
+# OUTPUT:
+#
+# I likeapple pie.
+# I like apple pie.
+# I like apple pie.
+# blink blink blink blink 
