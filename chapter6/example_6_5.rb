f400f57e084d45f16a3a8794eecce5f16ae8a811
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-20 17:38:41 -0600

    Add example_6_5.rb with its outputs in the file

---
+++ chapter6/example_6_5.rb
@@ -0,0 +1,53 @@
+# rand
+puts rand
+puts rand
+puts rand
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(1))
+puts(rand(1))
+puts(rand(1))
+puts(rand(9999999999999999999999999999999999999))
+puts('The weatherman said there is a')
+puts(rand(101).to_s + '% chance of rain,')
+puts('but you can never trust a weatherman.')
+
+# srand
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ''
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+
+
+# OUTPUT:
+#
+# 0.5718889722992905
+# 0.9008744903509566
+# 0.46652480083581394
+# 75
+# 91
+# 14
+# 0
+# 0
+# 0
+# 2850222787630216099660935859486257039
+# The weatherman said there is a
+# 18% chance of rain,
+# but you can never trust a weatherman.
+# 50
+# 21
+# 80
+# 15
+#
+# 50
+# 21
+# 80
+# 15
