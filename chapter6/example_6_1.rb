2bda0044ea472e43a02cfee12d628af7875cea8c
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-20 15:27:11 -0600

    Add more exmpl to the file with comments

--- chapter6/example_6_1.rb
+++ chapter6/example_6_1.rb
@@ -22,6 +22,22 @@ puts letters.capitalize
 puts ' a'.capitalize
 puts letters
 
+line_width = 50
+puts( 'Old Mother Hubbard'.center(line_width))
+puts( 'Sat in her cupboard'.center(line_width))
+puts( 'Eating her curds and whey,'.center(line_width))
+puts( 'When along came a spider'.center(line_width))
+puts( 'Who sat down beside her'.center(line_width))
+puts('And scared her poor shoe dog away.'.center(line_width))
+
+line_width = 40
+str = '--> text <--'
+puts(str.ljust( line_width))
+puts(str.center(line_width))
+puts(str.rjust( line_width))
+puts(str.ljust(line_width/2) + str.rjust(line_width/2))
+
+
 # OUTPUT:
 #
 # pots
@@ -40,3 +56,13 @@ puts letters
 # Aabbccddee
 #  a
 # aAbBcCdDeE
+#                   Old Mother Hubbard
+#                  Sat in her cupboard
+#               Eating her curds and whey,
+#                When along came a spider
+#                Who sat down beside her
+#           And scared her poor shoe dog away.
+# --> text <--
+#               --> text <--
+#                             --> text <--
+# --> text <--                --> text <--
