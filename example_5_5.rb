098d5b5a516429251ec12edc8a48c4ecd0716199
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-19 21:31:43 -0600

    Change the code and add the outs as commits to the file

--- example_5_5.rb
+++ example_5_5.rb
@@ -1,4 +1,11 @@
 puts 'Hello there, and what\'s your name?'
-name = 'Leila'.chomp
+name = gets.chomp
 puts 'Your name is ' + name + '? What a lovely name!'
 puts 'Pleased to meet you, ' + name + '. :)'
+
+# OUTPUT:
+#
+# Hello there, and what's your name?
+# Leila
+# Your name is Leila? What a lovely name!
+# Pleased to meet you, Leila. :)
