96ac887b0012ae37ba3c6504ef9424698390d325
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-20 18:02:49 -0600

    Add example_7_1.rb with its outputs as comments in the file

---
+++ chapter7/example_7_1.rb
@@ -0,0 +1,30 @@
+puts 1>2
+puts 1<2
+
+puts 5 >= 5
+puts 5 <= 4
+
+puts 1 == 1
+puts 2 != 1
+
+puts 'dog' < 'cat'
+
+puts 'bug lady' < 'Xander'
+puts 'bug lady'.downcase < 'Xander'.downcase
+
+puts 2 < 10
+puts '12' > '9'
+
+# OUTPUT:
+#
+# false
+# true
+# true
+# false
+# true
+# true
+# false
+# false
+# true
+# true
+# false
