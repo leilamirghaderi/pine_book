c83d618af7530f6a9d7fe687d1d327d667c0ddf1
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-22 19:12:26 -0600

    Add example_8_0.rb to chapter8

---
+++ chapter8/example_8_0.rb
@@ -0,0 +1,32 @@
+names = ['Ada', 'Belle', 'Chris']
+puts names
+puts
+puts names[0]
+puts names[1]
+puts names[2]
+puts names[3]
+
+other_peeps = []
+other_peeps[3] = 'beebee Meaner'
+other_peeps[0] = 'Ah-ha'
+other_peeps[1] = 'Seedee'
+other_peeps[0] = 'beebee Ah-ha'
+puts other_peeps
+
+# OUTPUT:
+#
+# Ada
+# Belle
+# Chris
+# (empty string)
+# Ada
+# # Belle
+# # Chris
+# (nil)
+
+# OUTPUT:
+#
+# beebee Ah-ha
+# Seedee
+# (nil)
+# beebee Meaner
