09d1a54cd512ed55f99f1d2097239dd2a099a132
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-27 17:54:27 -0600

    Add example_13_0.rb to chapter13

---
+++ chapter13/example_13_0.rb
@@ -0,0 +1,17 @@
+class Integer
+  def to_eng
+    if self == 5
+      english = 'five'
+    else
+      english = 'forty-two'
+    end
+    english
+  end
+end
+puts 5.to_eng
+puts 42.to_eng
+
+# OUTPUT
+#
+# five
+# forty-two
