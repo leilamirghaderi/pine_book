ad9522b5f5e69deb9c11975e38c8d558c1ef197b
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-27 17:22:29 -0600

    Add example_11_5.rb to chapter11

---
+++ chapter11/example_11_5.rb
@@ -0,0 +1,65 @@
+#number 1:
+buffy_quote_1 = '\'Kiss rocks\'?
+Why would anyone want to kiss...
+Oh, wait. I get it.'
+
+buffy_quote_2 = "'Kiss rocks'?\n" +
+                "Why would anyone want to kiss...\n" +
+                "Oh, wait. I get it."
+
+puts buffy_quote_1
+puts
+puts(buffy_quote_1 == buffy_quote_2)
+
+# number 2:
+puts "3...\n2...\n1...\nHAPPY NEW YEAR!"
+
+
+# number 3:
+puts 'single (\') and double (") quotes'
+puts "single (') and double (\") quotes"
+
+
+# number 4:
+name = 'Luke'
+zip = 90210
+puts "Name = #{name}, Zipcode = #{zip}"
+
+
+# number 5:
+puts "#{2 * 10**4 + 1} Leagues Under the Sea, THE REVENGE!"
+
+
+
+# OUTPUT (number 1)
+#
+# 'Kiss rocks'?
+# Why would anyone want to kiss...
+# Oh, wait. I get it.
+#
+# true
+
+
+# OUTPUT (number 2)
+#
+# true
+# 3...
+# 2...
+# 1...
+# HAPPY NEW YEAR!
+
+
+# OUTPUT (number 3)
+#
+# single (') and double (") quotes
+# single (') and double (") quotes
+
+
+# OUTPUT (number 4)
+#
+# Name = Luke, Zipcode = 90210
+
+
+# OUTPUT (number 5)
+#
+# 20001 Leagues Under the Sea, THE REVENGE!
