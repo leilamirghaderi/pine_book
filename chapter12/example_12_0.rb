ecba088e84c4394cf879e7616c089e5f3bb51fa5
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-27 17:32:05 -0600

    Add example_12_0.rb to chapter12

---
+++ chapter12/example_12_0.rb
@@ -0,0 +1,13 @@
+alpha = Array.new + [12345]
+beta = String.new + 'hello'
+karma = Time.new
+puts "alpha = #{alpha}"
+puts "beta = #{beta}"
+puts "karma = #{karma}"
+
+
+# OUTPUT
+#
+# alpha = [12345]
+# beta = hello
+# karma = 2019-01-27 17:30:48 -0600
