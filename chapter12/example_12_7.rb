994744e332ea988375ea1d35704593d24c37f053
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-27 17:52:20 -0600

    Add example_12_7.rb to chapter12

---
+++ chapter12/example_12_7.rb
@@ -0,0 +1,16 @@
+puts(42.class)
+puts("I'll have mayonnaise on mine!".class)
+puts(Time.new.class)
+puts(Time.class)
+puts(String.class)
+puts(Class.class)
+
+
+# OUTPUT
+#
+# Fixnum
+# String
+# Time
+# Class
+# Class
+# Clas
