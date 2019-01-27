a062263ac9e4bf821c7f3b032ecf9af61fe6f9dc
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-27 11:52:00 -0600

    Add try 1 to example_8_3 into chapter 8 folder

---
+++ chapter8/example_8_3.rb
@@ -0,0 +1,23 @@
+#try number one (Building and sorting an array):
+input_array = []
+while true
+  input_word = gets.chomp
+  if input_word.empty?
+    break
+  end
+  input_array.push input_word
+end
+puts input_array.sort
+
+
+
+
+# OUTPUT (Building and sorting an array):
+#
+# Tom
+# Megan
+# Leila
+#
+# Leila
+# Megan
+# Tom
