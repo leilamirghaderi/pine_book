b93f34162e13e454c93551c39898bbb0179287ff
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-29 21:33:21 -0600

    add try number one to example_10_3.rb to chapter10

---
+++ chapter10/example_10_3.rb
@@ -0,0 +1,23 @@
+# Try one (Shuffle):
+def make_shuffle input
+  puts input.sort_by{rand}
+end
+make_shuffle ([1, 2, 3, 4, 5])
+
+
+
+# Try two (Dictionary sort):
+
+
+
+# OUTPUT Try one (Shuffle):
+#
+# 2
+# 4
+# 5
+# 1
+# 3
+
+
+# OUTPUT Try two (Dictionary sort):
+#
