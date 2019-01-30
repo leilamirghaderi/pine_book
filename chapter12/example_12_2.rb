f5d92a1a804e7809a71bd69907f700cd44f803e1
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-01-29 21:34:33 -0600

    add try number one to example_12_2.rb to chapter12

---
+++ chapter12/example_12_2.rb
@@ -0,0 +1,7 @@
+# try one (One billion seconds):
+# I don't know the hour I was born in.
+puts (Time.gm(1990,4,3,12,0))+10**9
+
+# OUTPUT try one (One billion seconds):
+#
+# 2021-12-10 13:46:40 UTC
